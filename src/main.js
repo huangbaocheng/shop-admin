10b96ce88e5509fd379ef6f3896dfccee9318eb0
huangbaocheng <786886565@qq.com>
2019-06-06 12:30:01 +0800

    第一次提交vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+//导入组件
+import ElementUI from 'element-ui';
+// element的样式
+import 'element-ui/lib/theme-chalk/index.css';
+// element: 2.注册element插件
+Vue.use(ElementUI);
+
+
 
 Vue.config.productionTip = false
 
